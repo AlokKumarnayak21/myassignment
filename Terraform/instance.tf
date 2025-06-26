9d38764ff32880d9d9257bf5d4763af6efc0fbdb
AlokKumarnayak21 <tel2alok21@gmail.com>
2025-06-26 10:25:04 +0530

    1st push

---
+++ Terraform/instance.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = var.key_pair
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "Demo-Alok"
+  }
+}
